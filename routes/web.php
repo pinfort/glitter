cab6afeccf92d09fa4188fb8f14d4d2a10fa663c
pinfort <ptg@nijitei.com>
2018-05-10 21:43:35 +0900

    auth route変更

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::group(['prefix' => 'auth'], function () {
+    Auth::routes();
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
