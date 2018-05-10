e9f44c7f9fec6b3a3bcbf847b641e28199bc760c
pinfort <ptg@nijitei.com>
2018-05-10 22:20:11 +0900

    add social login logic

---
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Contracts\User as ProviderUser;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\LinkedSocialAccount;
+
+class SocialAccountController extends Controller
+{
+    /**
+     * Redirect the user to the authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return \Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+
+        try {
+            $user = \Socialite::with($provider)->user();
+        } catch (\Exception $e) {
+            return redirect('/auth/login');
+        }
+
+        $authUser = $this->findOrCreate(
+            $user,
+            $provider
+        );
+
+        auth()->login($authUser, true);
+
+        return redirect()->to('/home');
+    }
+
+    public function findOrCreate(ProviderUser $providerUser, $provider)
+    {
+        $account = LinkedSocialAccount::where('provider_name', $provider)
+                   ->where('provider_id', $providerUser->getId())
+                   ->first();
+
+        if ($account) {
+            return $account->user;
+        } else {
+
+            $user = Auth::user();
+
+            
+            if ((!$user) or is_null($user)) {
+                if ($provider === 'twitter') {
+                    $user = User::create([
+                        'email' => $providerUser->getEmail(),
+                        'name'  => $providerUser->getName(),
+                    ]);
+                } else {
+                    redirect(route('login'));
+                }
+            }
+
+            $user->accounts()->create([
+                'provider_id'   => $providerUser->getId(),
+                'provider_name' => $provider,
+            ]);
+
+            return $user;
+
+        }
+    }
+}
--- app/LinkedSocialAccount.php
+++ app/LinkedSocialAccount.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class LinkedSocialAccount extends Model
 {
-    //
+    protected $fillable = [ 'provider_name', 'provider_id' ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
