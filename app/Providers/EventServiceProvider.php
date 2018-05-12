b8cc25d13ffe83d7818fe044102aaf5e40d04044
pinfort <ptg@nijitei.com>
2018-05-12 12:32:35 +0900

    pinfort/gitlabを使うのでなく継承に変更

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,7 +17,7 @@ class EventServiceProvider extends ServiceProvider
             'App\Listeners\EventListener',
         ],
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            'SocialiteProviders\\GitLab\\GitLabExtendSocialite@handle',
+            'App\\Lib\\GitLabApi\\GitLabExtendSocialite@handle',
         ],
     ];
 
