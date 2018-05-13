0ba01e77e0ef89d42f9f73230dd9a6d19b67627f
pinfort <ptg@nijitei.com>
2018-05-14 01:51:37 +0900

    ツイートフォーマット変更

--- config/glitter.php
+++ config/glitter.php
@@ -8,6 +8,6 @@ return [
      * %3$d events count
      * %4$d commits count
      */
-    'format' => '%1$s さんの %2$s のgitlabでの活動は %3$d 件でした。\n\nコミット数 %4$d 件\n#glitter_gitlab',
+    'format' => '%1$s さんの %2$s のgitlabでの活動は %3$d 件でした。'."\n\nコミット数".' %4$d '."件\n#glitter_report",
 
 ];
