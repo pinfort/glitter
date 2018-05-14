138e102eff2429ccd465b5a44b506cc2c3c06e84
pinfort <ptg@nijitei.com>
2018-05-14 14:47:47 +0900

    トップページカスタマイズ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,15 +78,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Glitter
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Start with Login</a>
+                    @endauth
                 </div>
             </div>
         </div>
